49c28eb7b5120a8edf199561a67d3f3888d1d749
Aritra Dam <aritrodam773@gmail.com>
2023-03-06 13:23:25 +0530

    hardhat test 2

--- hardhat learn/test/token.js
+++ hardhat learn/test/token.js
@@ -14,5 +14,16 @@ describe("Token contract", function () {
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
 
     });
+    it("Should Transfer Tokens between Accounts", async function () {
+        const [owner, addr1, addr2] = await ethers.getSigners();
 
+        const Token = await ethers.getContractFactory("Token");
+        const hardhatToken = await Token.deploy();
+
+        await hardhatToken.transfer(addr1.address, 10);
+        expect(await hardhatToken.checkBalance(addr1.address)).to.equal(10);
+        await hardhatToken.connect(addr1).transfer(addr2.address, 5);
+        expect(await hardhatToken.checkBalance(addr2.address)).to.equal(5);
+
+    });
 });
