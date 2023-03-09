369883959b0125e8fd6cdb786039b94b2529a295
Aritra Dam <aritrodam773@gmail.com>
2023-03-09 17:04:49 +0530

    hardhat debugging import hardhat/console.sol

--- hardhat learn/contracts/Token.sol
+++ hardhat learn/contracts/Token.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
+import "hardhat/console.sol";
 
 contract Token {
     string public name = "HardHat Token";
@@ -15,6 +16,8 @@ contract Token {
     }
 
     function transfer(address _to, uint amount) external {
+        console.log("  '**'Sender Balance:", balances[msg.sender]);
+        console.log("  '**'Sending to:", _to);
         require(balances[msg.sender] >= amount, "Not enough token");
         balances[msg.sender] -= amount;
         balances[_to] += amount;
