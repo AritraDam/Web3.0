c629b0d3731d8824915c3f51c92d4fe4f452c3d3
Aritra Dam <aritrodam773@gmail.com>
2023-03-11 00:51:54 +0530

    General template for any Dapp (state that contains provider,signer,contract instance)

--- chai_Dapp/client/src/App.js
+++ chai_Dapp/client/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import abi from "./contracts/chai.json";
 import './App.css';
-
 function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0x026f6a86fa66d02ccf84238d556c95b962fb395a";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+        if (ethereum) {
+          const account = await ethereum.request({ method: "eth_requestAccounts" })
+        }
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+        setState({ provider, signer, contract });
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+    connectWallet();
+  }, []);
+  console.log(state);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
